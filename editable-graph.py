78aec0cf1bc12ab28e7e7cea445d9341dab60e81
jf <jf@poiu>
2020-04-10 18:47:56 +0200

    Simplified way of drawing arrows + handling mutligraphs

--- editable-graph.py
+++ editable-graph.py
@@ -2,6 +2,7 @@ from ipycanvas import Canvas, MultiCanvas, hold_canvas
 from ipywidgets import Label, VBox, HBox, Output, Button, Dropdown, ColorPicker
 from random import randint, randrange
 from math import pi
+from itertools import chain
 
 class GraphWithEditor():
     output = Output(layout={'border': '1px solid black'})
@@ -27,6 +28,20 @@ class GraphWithEditor():
             'arrow_tip_width': 15,
             'arrow_tip_height': 8
         }
+
+        def draw_arrow(canvas):
+            '''Draw an arrow at with tip at (0,0), pointing to the left.'''
+            a_x = self.drawing_parameters['arrow_tip_width']
+            a_y = self.drawing_parameters['arrow_tip_height']
+            canvas.begin_path()
+            canvas.move_to(0, 0)
+            canvas.line_to(a_x, a_y)
+            canvas.line_to(0.75*a_x, 0)
+            canvas.line_to(a_x, -a_y)
+            canvas.move_to(0, 0)
+            canvas.fill()
+        self.drawing_parameters['draw_arrow'] = draw_arrow
+        
         # Radii, positions and colors of the vertices on the drawing
         self.vertex_radii = dict()
 
@@ -174,12 +189,20 @@ class GraphWithEditor():
 
     def add_edge(self, u, v):
         '''Add an edge to the undelying graph and draw it on the main canvas.'''
-        self.graph.add_edge(u,v)
-        # We draw the edge and the nodes shapes on top of it
-        with hold_canvas(self.canvas):
-            self._draw_edges([(self.selected_vertex, v, None)])
-            self._draw_vertex(v)
-            self._draw_vertex(self.selected_vertex)
+
+        if (self.graph.allows_multiple_edges() and
+            (self.graph.has_edge(u,v) or self.graph.has_edge(v,u))):
+            self.graph.add_edge(u,v)
+            # As there are other arcs already, we need to redraw everything
+            self._draw_graph()
+        else:
+            self.graph.add_edge(u,v)
+            # We draw the new edge and the nodes shapes on top of what is
+            # already drawn
+            with hold_canvas(self.canvas):
+                self._draw_edges([(self.selected_vertex, v, None)])
+                self._draw_vertex(v)
+                self._draw_vertex(self.selected_vertex)
 
     def _draw_vertex(self, v, canvas=None, color=None):
         x, y = self.pos[v]
@@ -252,23 +275,37 @@ class GraphWithEditor():
         with hold_canvas(canvas):
             if neighbors: # We also redraw the incident edges and the neighbors
                 if self.graph.is_directed():
-                    # We independently consider in- and out-edges
-                    self._draw_edges((e for e in self.graph.incoming_edge_iterator(v)), canvas=canvas)
-                    self._draw_edges((e for e in self.graph.outgoing_edge_iterator(v)), canvas=canvas)
+                    if self.graph.allows_multiple_edges():
+                        # In this case we must iterate over all edges, so that
+                        # we draw them in the same order as when we draw the
+                        # full graph...
+                        incident_edges = ((u1,u2,l)
+                                          for (u1,u2,l) in self.graph.edge_iterator()
+                                          if v == u1 or v == u2)
+                    else:
+                        # We chain the iterators in order to make a unique call
+                        # to _draw_edges, which is important when dealing with
+                        # digraphs with multiple edges
+                        incident_edges = chain(
+                            (e for e in self.graph.incoming_edge_iterator(v)),
+                            (e for e in self.graph.outgoing_edge_iterator(v)))
+                    # Finally we draw those directed edges
+                    self._draw_edges(incident_edges, canvas=canvas)
                 else:
-                    self._draw_edges((e for e in self.graph.edge_iterator(vertices=v)), canvas=canvas)
+                    self._draw_edges((e for e in self.graph.edge_iterator(v)), canvas=canvas)
                 self._draw_vertices((u for u in self.graph.neighbor_iterator(v)), canvas=canvas)
+                
             self._draw_vertex(v, canvas=canvas, color=color)
             if self.selected_vertex is not None and highlight:
                 self._highlight_vertex(self.selected_vertex)
 
-    def _draw_edge(self, e, canvas=None, curve=0):
+    def _draw_edge(self, e, canvas=None, curve=None, middle_arrow=False):
         u, v, l = e
         pos_u, pos_v = self.pos[u], self.pos[v]
         if canvas is None:
             canvas = self.canvas
 
-        if curve:
+        if curve is not None:
             canvas.begin_path()
             canvas.move_to(*pos_u)
             # u-v distance:
@@ -285,52 +322,117 @@ class GraphWithEditor():
                   huv[1] + nuv[1] * curve)
             canvas.quadratic_curve_to(*cp, *pos_v)
             canvas.stroke()
+
+            if self.graph.is_directed():
+                # If the graph is directed, we also have to draw the arrow tip
+                # In this case (curved edge), we draw the arrow at mid-edge.
+                dist_min = self.get_radius(v) + self.get_radius(u)
+                # We only do it if the vertex shapes do not overlap.
+                if (abs(pos_u[0] - pos_v[0]) > dist_min
+                    or abs(pos_u[1] - pos_v[1]) > dist_min):
+                    canvas.save()
+                    # Move the canvas so that v lies at (0,0) and
+                    # rotate it so that v-y is on the x>0 axis:
+                    canvas.translate(*huv)
+                    canvas.rotate(float(atan2(pos_u[1] - pos_v[1],
+                                              pos_u[0] - pos_v[0])))
+                    # Move to the point where the edge joins v's shape and
+                    # draw the arrow there:
+                    canvas.translate(-self.drawing_parameters['arrow_tip_width']/2, curve/2)
+                    self.drawing_parameters['draw_arrow'](canvas)
+                    canvas.restore()
+                
         else:
             canvas.begin_path()
             canvas.move_to(*pos_u)
             canvas.line_to(*pos_v)
             canvas.stroke()
             
-        if self.graph.is_directed():
-            # If the graph is directed, we also have to draw the arrow tip
-            radius_v = self.get_radius(v)
-            dist_min = radius_v + self.get_radius(v)
+            if self.graph.is_directed():
+                # If the graph is directed, we also have to draw the arrow tip
+                # We draw it at the end of the edge (as usual)
+                radius_v = self.get_radius(v)
+                dist_min = radius_v + self.get_radius(u)
 
-            # We only do it if the vertex shapes do not overlap.
-            if (abs(pos_u[0] - pos_v[0]) > dist_min
-                or abs(pos_u[1] - pos_v[1]) > dist_min):
-                canvas.save()
-                canvas.translate(*pos_v)
-                canvas.rotate(float(atan2(pos_u[1] - pos_v[1],
-                                          pos_u[0] - pos_v[0])))
-                a_x = self.drawing_parameters['arrow_tip_width']
-                a_y = self.drawing_parameters['arrow_tip_height']
-                radius = self.get_radius(v)
-                canvas.begin_path()
-                canvas.move_to(radius_v, 0)
-                canvas.line_to(radius_v + a_x, a_y)
-                canvas.line_to(radius_v + 0.75*a_x, 0)
-                canvas.line_to(radius_v + a_x, -a_y)
-                canvas.move_to(radius_v, 0)
-                canvas.fill()
-                canvas.restore()
+                # We only do it if the vertex shapes do not overlap.
+                if (abs(pos_u[0] - pos_v[0]) > dist_min
+                    or abs(pos_u[1] - pos_v[1]) > dist_min):
 
-    def _draw_edges(self, edges=None, canvas=None, color='black'):
-        # To use within a "with hold_canvas" block, preferably
-        if edges is None:
-            edges = self.edge_iterator()
+                    canvas.save()
+                    # Move the canvas so that v lies at (0,0) and
+                    # rotate it so that v-y is on the x>0 axis:
+                    canvas.translate(*pos_v)
+                    canvas.rotate(float(atan2(pos_u[1] - pos_v[1],
+                                              pos_u[0] - pos_v[0])))
+                    # Move to the point where the edge joins v's shape and
+                    # draw the arrow there:
+                    canvas.translate(self.get_radius(v), 0)
+                    self.drawing_parameters['draw_arrow'](canvas)
+                    canvas.restore()
+
+    def _draw_edges(self, edges, canvas=None, color='black'):
+        # 
+        '''
+        Draw the edges contained in `edges` on canvas `canvas` and
+        in color `color`.
+
+        To use within a `with hold_canvas` block, preferably.
+        If `edges` is not specified, draw the edges of the whole graph.
+        If `canvas` is not specified, draw on self.canvas.
+
+        WARNING: When dealing with oriented multigraphs, the following should be
+        applied: for every vertices u and v, all the edges between u and v
+        (in either direction) should be drawn within the same call to
+        _draw_edges (otherwise some edges might be drawn on top of each other).
+        '''
+        
         if canvas is None:
             canvas = self.canvas
+
         canvas.stroke_style = color
         canvas.line_width = 3
+            
+        if not self.graph.allows_multiple_edges():
+            for e in edges:
+                self._draw_edge(e, canvas=canvas)
+        else:
+            # To count how many times an edge (u,v) has been drawn
+            # so far
+            seen_edges = dict()
+            # To remember which of (u,v) and (v,u) was drawn the latest
+            seen_last = dict()
+
+            def get_curve(e):
+                '''Return the curve of the edge e.'''
+                u, v, _ = e
+                # How many times we saw an edge between these vertices so far:
+                cur_mul = seen_edges.get((u, v), 0) + seen_edges.get((v, u), 0)
+                seen_edges[(u,v)] = 1 + seen_edges.get((u, v), 0) 
+                if not cur_mul:
+                    # No edge between u and v has been drawn so far
+                    seen_last[(u,v)] = True
+                    seen_last[(v,u)] = False
+                    return 0
+                else:
+                    # Among edges of the forms (u,v) and (v,u),
+                    # was an edge of the form (u,v) seen last?
+                    uv_last = seen_last[(u,v)]
+                    seen_last[(u,v)] = True
+                    seen_last[(v,u)] = False
+                    factor = -1 if uv_last else 1
+                    if is_even(cur_mul):
+                        return factor * cur_mul * 15
+                    else:
+                        return -factor * (cur_mul+1) * 15
+
         for e in edges:
-            self._draw_edge(e, canvas=canvas)
+                self._draw_edge(e, canvas=canvas, curve=get_curve(e))
 
     def _draw_graph(self):
 
         with hold_canvas(self.canvas):
             self.canvas.clear()
-            self._draw_edges()
+            self._draw_edges(self.graph.edge_iterator())
             self._draw_vertices()
             if self.selected_vertex is not None:
                 self._highlight_vertex(self.selected_vertex)
@@ -344,7 +446,7 @@ class GraphWithEditor():
         self.output_text("Click at (" + str(pixel_x) + ", " + str(pixel_y) + ")")
 
         # Find the clicked node (if any):
-        for v in self.vertex_iterator():
+        for v in self.graph.vertex_iterator():
             mark_x, mark_y = self.pos[v]
             mark_radius = self.get_radius(v)
 
@@ -353,20 +455,14 @@ class GraphWithEditor():
                 abs(pixel_y - mark_y) < mark_radius):
                 # The user clicked on vertex v!
 
-                if self.selected_vertex is not None:
-                    if self.selected_vertex == v:
-                        # We unselect vertex v
-                        self.selected_vertex = None
-                        self.output_text("No vertex selected")
-                        self._redraw_vertex(v) # Redraw without highlight
-                        return
-                    else:
-                        # A node was selected and we clicked on a new node v:
-                        # we link v and the previously selected vertex
-                        self.add_edge(self.selected_vertex, v)
-                        self.output_text("Added edge from " + str(self.selected_vertex) + " to " + str(v))
-                        self.selected_vertex = None
-                        return
+                if self.selected_vertex is not None and self.selected_vertex != v:
+                    # A node was selected and we clicked on a new node v:
+                    # we link v and the previously selected vertex
+                    self.add_edge(self.selected_vertex, v)
+                    self.output_text("Added edge from " + str(self.selected_vertex) + " to " + str(v))
+                    self.selected_vertex = None
+                    self._redraw_vertex(v, neighbors=False) # Redraw v without highlight
+                    return
 
                 # At this point, no vertex is currently selected
                 self.dragged_vertex = v
@@ -375,14 +471,15 @@ class GraphWithEditor():
                 with hold_canvas(self.multi_canvas):
                     # On the main canvas we draw everything,
                     # except the dragged vertex and the edges
-                    # incident to it. We draw the rest on the
-                    # interact canvas.
+                    # incident to it.
                     self.canvas.clear()
+                    self._draw_edges(((u1, u2, l)
+                                      for (u1, u2, l) in self.graph.edge_iterator()
+                                      if (v != u1 and v != u2)))
+                    self._draw_vertices((u for u in self.graph.vertex_iterator()
+                                        if u != v))
+                    # We draw the rest on the interact canvas.
                     self.interact_canvas.clear()
-                    self._draw_edges(((u1, u2, l) for (u1, u2, l) in self.edge_iterator()
-                                    if v is not u1 and v is not u2))
-                    self._draw_vertices((u for u in self.vertex_iterator()
-                                        if u is not v))
                     self._redraw_vertex(v, canvas=self.interact_canvas,
                                         neighbors=True,
                                         highlight=False,
@@ -394,7 +491,7 @@ class GraphWithEditor():
         if self.selected_vertex is not None:
             # If a vertex is selected and we click on the background, we
             # un-select it
-            self._redraw_vertex(self.selected_vertex, highlight=False) # Redraw it without highlighting
+            self._redraw_vertex(self.selected_vertex, highlight=False, neighbors=False) # Redraw it without highlighting
             self.selected_vertex = None
         else:
             # Otherwise, we add a new vertex
@@ -424,17 +521,19 @@ class GraphWithEditor():
     @output.capture()
     def mouse_up_handler(self, pixel_x, pixel_y):
         if self.dragged_vertex is not None:
-            # If we dragged the vertex very close to its initial position
-            # we actually wanted to select it
+            # If we dragged the vertex very close to its initial position,
+            # we actually wanted to (un)select it
             if (abs(pixel_x - self.initial_click_pos[0]) < 10
                 and abs(pixel_y - self.initial_click_pos[1]) < 10):
-                assert self.selected_vertex is None
-                self.selected_vertex = self.dragged_vertex
-                self.output_text("Selected vertex " + str(self.selected_vertex))
-                self.color_selector.value = self.colors[self.selected_vertex]
-                self._draw_graph()
-                #self._redraw_vertex(self.selected_vertex)
-                
+                if self.selected_vertex is None:
+                    self.selected_vertex = self.dragged_vertex
+                    self.output_text("Selected vertex " + str(self.selected_vertex))
+                    self.color_selector.value = self.colors[self.selected_vertex]
+                else:
+                    self.selected_vertex = None
+
+                self._redraw_vertex(self.dragged_vertex)
+                self.dragged_vertex = None
             else:
                 self.output_text("Done draging vertex.")
                 self._draw_graph()
