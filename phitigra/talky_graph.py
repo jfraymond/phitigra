89908d8e592690bd2e178c364cf605f0257f1883
jf <jf@mnbv>
2020-06-25 17:21:22 +0200

    doc in TalkyGraph

--- phitigra/talky_graph.py
+++ phitigra/talky_graph.py
@@ -1,22 +1,49 @@
-from sage.graphs.graph import Graph, DiGraph
+# -*- coding: utf-8 -*-
+r"""
+TalkyGraph
 
-'''
-A class to add notifications to the class Graph and DiGraph when vertices and
-edges are added or deleted.
-'''
+This module defines the ``TalkyGraph`` class, that subclasses Sage's ``Graph`
+class to trigger a notification when changes are made to the instances.
+"""
+
+# ****************************************************************************
+#
+#       Copyright (C) 2020      Jean-Florent Raymond <j-florent.raymond@uca.fr>
+#
+# ****************************************************************************
+
+from sage.graphs.graph import Graph
 
 
 class TalkyGraph(Graph):
-    '''
-    Add notification for created or deleted edges or vertices of a
-    sage ``Graph``.
-    '''
+    """
+    Subclass of Sage's ``Graph`` class that behaves exactly the same, except
+    that a callback is called when changes are made to the instance (addition
+    and deletion of vertices).
+
+    EXAMPLES:
+        sage: def f(s): print("Notification: " + str(s))
+        sage: g = TalkyGraph(notify_callback=f)
+        sage: g.add_vertex(0)
+        Notification: add_vertex
+        sage: g.add_vertex()
+        Notification: add_vertex
+        1
+        sage: g.add_edge(0, )
+        Notification: add_edge
+        sage: g.delete_vertex(1)
+        Notification: delete_vertex
+
+        sage: h = TalkyGraph()
+        sage: h.add_vertex()
+        0
+    """
+
     def __init__(self, *args, **kwargs):
         notify = kwargs.pop('notify_callback', lambda x:None)
         super().__init__(*args, **kwargs)
         # We define notify_change after the call to Graph's constructor so that
-        # no notification (asking to redraw the graph) is done until the graph
-        # is completely built
+        # no notification is done until the graph is completely built
         self.notify_change = notify
 
     def add_vertex(self, *args, **kwargs):
@@ -55,11 +82,11 @@ class TalkyGraph(Graph):
         return r
 
     def notify_change(self, s):
-        '''
+        """
         The function called whenever a vertex or edge deletion or addition is
         made.
 
         By default empty, this function can be specified when creating a new
         instance
-        '''
+        """
         return
