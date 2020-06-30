fa16f1a4d21a37f68d3c27f3aa6db87e5644f93e
jf <jf@mnbv>
2020-06-30 16:26:46 +0200

    _repr_ function for TalkyGraph + examples and tests

--- phitigra/talky_graph.py
+++ phitigra/talky_graph.py
@@ -34,24 +34,83 @@ class TalkyGraph(Graph):
         sage: g.delete_vertex(1)
         Notification: delete_vertex
 
+    The default callback is the function that does nothing::
+ 
         sage: h = TalkyGraph()
         sage: h.add_vertex()
         0
+
+    TESTS:
+
+    The construction of the graph does not trigger any notification::
+
+        sage: def f(s): print("Notification: " + str(s))
+        g = TalkyGraph(2, notify_callback=f); g
+        Graph on 2 vertices
     """
 
     def __init__(self, *args, **kwargs):
+        # Get the callback if given or use a no-op function otherwise
         notify = kwargs.pop('notify_callback', lambda x:None)
         super().__init__(*args, **kwargs)
-        # We define notify_change after the call to Graph's constructor so that
-        # no notification is done until the graph is completely built
+
+        # notify_change refers to the callback that functions modifying the
+        # graph will call. We define it after the call to Graph's constructor
+        # so that no notification is done until the graph is completely built
         self.notify_change = notify
 
+    def _repr_(self):
+        """
+        Return a string representation of the graph.
+
+        EXAMPLES::
+
+            sage: g = TalkyGraph(5); g
+            TalkyGraph with base Graph on 5 vertices
+            sage: g = TalkyGraph(graphs.PetersenGraph()); g
+            TalkyGraph with base Petersen graph: Graph on 10 vertices
+        """
+        return "TalkyGraph with base " + Graph._repr_(self)
+        
     def add_vertex(self, *args, **kwargs):
+        r"""
+        Create an isolated vertex.
+
+        Same as :meth:`Graph.add_vertex` but followed by a call to
+        :meth:`notify_change`.
+
+        TESTS::
+
+            sage: def f(s): print("The graph changed: " + str(s))
+            sage: g = TalkyGraph(notify_callback=f)
+            sage: g.add_vertex(0)
+            The graph changed: add_vertex
+            sage: g.add_vertex()
+            The graph changed: add_vertex
+            1
+        """
         r = Graph.add_vertex(self, *args, **kwargs)
         self.notify_change('add_vertex')    # , *args, **kwargs)
         return r
 
     def delete_vertex(self, *args, **kwargs):
+        r"""
+        Delete vertex, removing all incident edges.
+
+        Same as :meth:`Graph.delete_vertex` but followed by a call to
+        :meth:`notify_change`.
+
+        TESTS::
+
+            sage: def f(s): print("The graph changed: " + str(s))
+            sage: g = TalkyGraph(2, notify_callback=f)
+            sage: g.delete_vertex(1)
+            The graph changed: delete_vertex
+            sage: g.delete_vertex(1)
+            Traceback (most recent call last):
+            ...
+            ValueError: vertex (1) not in the graph
+        """
         r = Graph.delete_vertex(self, *args, **kwargs)
         self.notify_change('delete_vertex')    # , *args, **kwargs)
         return r
